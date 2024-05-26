afba56287e042bf96461b12a38c81039b0086895
Sama Ayman <121590157+Sama-Sokkar@users.noreply.github.com>
2024-05-26 03:30:28 +0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
+import StarRating from "./StarRating";
+import "./index.css";
+import App from "./App";
+
+// function Test() {
+//   const [movieRating, setMovieRating] = useState(0);
+
+//   return (
+//     <div>
+//       <StarRating color="blue" maxRating={5} onSetRating={setMovieRating} />
+//       <p>This movie was rated {movieRating} stars</p>
+//     </div>
+//   );
+// }
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+    {/* <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+    />
+    <StarRating size={24} color="red" className="test" defaultRating={1} />
+    <Test /> */}
+  </React.StrictMode>
+);
